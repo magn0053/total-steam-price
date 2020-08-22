d292bdc7e3470cfc917fa50170b9d7abb5b0b905
Magnus Vestergaard <Magnusvestergaard@live.dk>
2020-08-22 15:08:42 +0200

    Further explanation of the project

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const   mainPage = require('./web/mainPage')
-        steamApi = require('./api/steam')
+        ,dashboard = require('./web/dashboard')
+        ,steamApi = require('./api/steam')
         ,express = require('express')
         ,app = express()
         ,port = 4004
 
 
         
+// The part for most users
 app.use('/', mainPage)
+
+// Handling the dashboard for controlling the application
+app.use('/dashboard', dashboard)
+
+// All the APIs
 app.use('/api/steam', steamApi)
 
 app.listen(port, () => console.log(`App is listening at http://localhost:${port}`))
